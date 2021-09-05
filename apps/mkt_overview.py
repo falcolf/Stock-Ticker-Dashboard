cb1fb8734e9c5b59bef18d5f11e179cbf04f4fa6
Parth Agarwal <parth1102007@gmail.com>
2021-09-05 20:13:30 +0530

    Added styling mkt overview top

--- apps/mkt_overview.py
+++ apps/mkt_overview.py
@@ -1,4 +1,5 @@
 from datetime import date
+import datetime
 import pandas as pd
 import numpy as np
 import plotly.offline as pyo
@@ -23,24 +24,23 @@ from lxml import html as html_parser
 chrome_options = Options()
 chrome_options.add_argument("--headless")
 driver = webdriver.Chrome(ChromeDriverManager().install(), options=chrome_options)
-options = webdriver.ChromeOptions();
-options.add_argument('headless');
-
-driver.get('https://www.tickertape.in/market-mood-index')
-html_src = html_parser.fromstring(driver.page_source)
-mmi_value = float(html_src.xpath('//*[@id="app-container"]/div/div[1]/div[1]/div/div[2]/span')[0].text)
+options = webdriver.ChromeOptions()
+options.add_argument('headless')
 
 layout = html.Div([
                 html.Div([
-                            daq.Gauge(
-                                showCurrentValue=True,
-                                color={"gradient":True,"ranges":{"green":[0,30],"yellow":[30,50],"orange":[50,70],"red":[70,100]}},
-                                value=mmi_value,
-                                label='MMI',
-                                max=100,
-                                min=0,
-                            )              
+                            dcc.Interval(
+                                    id='interval-component',
+                                    interval=1000 * 1 * 60 * 60 * 6, # in milliseconds --> 6 hours
+                                    n_intervals=0
+                                ),
+                            dbc.Row([
+                                        dbc.Col(id = 'mmi_stats', width = 6),
+                                        dbc.Col(dcc.Graph(id = 'world_indices'))
+                                                
+                                ], style = {"padding":"2rem 2rem 2rem 2rem"})              
                 ]),
+                html.Hr(),
                 html.Div([
                             dbc.FormGroup([
                                             dbc.RadioItems(
@@ -51,7 +51,7 @@ layout = html.Div([
                                                 value='nifty',
                                                 id="index",
                                                 inline = True,
-                                                style = {"padding":"1rem 1rem 1rem 1rem"}
+                                                style = {"padding":"1rem 0.5rem 0.5rem 0.5rem"}
                                             ),
                                         ]),
                             dbc.Row(
@@ -63,6 +63,54 @@ layout = html.Div([
                 ])
                 
             ])
+
+@app.callback(
+                [
+                    Output('mmi_stats', 'children'),
+                    Output('world_indices','figure')],
+                [Input('interval-component', 'n_intervals')]
+            )
+def publish_top_stats(n):
+
+    driver.get('https://www.tickertape.in/market-mood-index')
+    html_src = html_parser.fromstring(driver.page_source)
+    mmi_value = float(html_src.xpath('//*[@id="app-container"]/div/div[1]/div[1]/div/div[2]/span')[0].text)
+
+    mmi_indicator = daq.Gauge(
+                    showCurrentValue=True,
+                    color={"gradient":True,"ranges":{"green":[0,30],"yellow":[30,50],"orange":[50,70],"red":[70,100]}},
+                    value=mmi_value,
+                    label='MMI',
+                    max=100,
+                    min=0,
+                )
+
+    def get_ticker_change_fig(ticker, row, col):
+        start, end, high, low = get_ticker_stats(ticker, period = '5d')
+        return go.Indicator(
+                        mode = "delta",
+                        value = end,
+                        title = {"text": "{}<br><span style='font-size:0.8em;color:gray'>5d Change</span>".format(ticker), 'font':{'size':15} },
+                        delta = {'reference': start, 'relative': True, 'font':{'size':20}},
+                        domain = {'row': row, 'column': col}
+                    )
+    
+
+    world_indices = list()
+    world_indices.append(get_ticker_change_fig(ticker = '^NSEI', row = 0, col = 0))
+    world_indices.append(get_ticker_change_fig(ticker = '^BSESN', row = 0, col = 1))
+    world_indices.append(get_ticker_change_fig(ticker = '^N225', row = 1, col = 0))
+    world_indices.append(get_ticker_change_fig(ticker = 'LSEG.L', row = 1, col = 1))
+    world_indices.append(get_ticker_change_fig(ticker = '^NDX', row = 2, col = 0))
+    world_indices.append(get_ticker_change_fig(ticker = '^GSPC', row = 2, col = 1))
+
+    world_indices_fig = {
+                'data': world_indices,
+                'layout': go.Layout(hovermode='closest', grid = {'rows': 3, 'columns': 2, 'pattern': "independent"}, height=400)
+            }  
+
+    return mmi_indicator, world_indices_fig
+
 
 @app.callback(
                 Output('index_stats', 'figure'),
