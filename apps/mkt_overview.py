84211748650da6e3b0449a3d84596d16f04e942f
Parth Agarwal <parth1102007@gmail.com>
2021-10-04 01:12:08 +0530

    init launch

--- apps/mkt_overview.py
+++ apps/mkt_overview.py
@@ -19,12 +19,13 @@ from utils.stock_data import get_stock_data, get_ticker_stats
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from webdriver_manager.chrome import ChromeDriverManager
+from webdriver_manager.firefox import GeckoDriverManager
 from lxml import html as html_parser
 
 chrome_options = Options()
 chrome_options.add_argument("--headless")
-driver = webdriver.Chrome(ChromeDriverManager().install(), options=chrome_options)
-#driver = webdriver.Chrome("./chromedriver_linux", options=chrome_options)
+#driver = webdriver.Chrome(ChromeDriverManager().install(), options=chrome_options)
+driver = webdriver.Firefox(GeckoDriverManager().install(), options=chrome_options)
 options = webdriver.ChromeOptions()
 options.add_argument('headless')
 
